f4530ad79236527f85e6a00f6f6063d7cc656af4
Lwazi Mabota <greninja71@gmail.com>
2022-06-27 17:49:45 -0700

    Added div to center text elements in home page

--- src/home.js
+++ src/home.js
@@ -1,24 +1,26 @@
 function initialPageLoad(){
     const contentContainer = document.querySelector("#content");
+    const homeItems = document.querySelector("#home-items");
 
     const burgerBackground = document.createElement("div");
     burgerBackground.classList.add("burger-background");
     contentContainer.appendChild(burgerBackground);
+    burgerBackground.appendChild(homeItems);
     
     const heading = document.createElement("h2");
     heading.textContent = "Best Burgers in Bremerton";
     heading.classList.add("heading-burger");
-    burgerBackground.appendChild(heading);
+    homeItems.appendChild(heading);
 
     const burgerParagraph = document.createElement("p");
     burgerParagraph.classList.add("burger-paragraph");
     burgerParagraph.textContent = "Locally Sourced x Crafted with Love";
-    burgerBackground.appendChild(burgerParagraph);
+    homeItems.appendChild(burgerParagraph);
 
     const burgerButton = document.createElement("button");
     burgerButton.classList.add("burger-button");
     burgerButton.textContent = "Order Now";
-    burgerBackground.appendChild(burgerButton);
+    homeItems.appendChild(burgerButton);
 }
 
 export { initialPageLoad }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { initialPageLoad}  from './home.js';
 import './style.css'
 
 
-console.log("Your mom!")
+
 initialPageLoad();
