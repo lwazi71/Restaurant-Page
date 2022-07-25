7c8f7ad50596cee4929cba3479bbab38991abaf2
Lwazi Mabota <greninja71@gmail.com>
2022-07-25 11:50:21 -0700

    Refactored header changes allowing for burger menu to appear on wider viewports

--- src/index.js
+++ src/index.js
@@ -8,27 +8,11 @@ import {  contactPageLoad, contactPage } from './contact.js';
 
 import './style.css';
  
- 
- const menuSection = document.getElementById("menu");
- const aboutSection = document.getElementById("about");
- const contactSection = document.getElementById("contact");
- const linkPages = document.querySelectorAll(".page");
- 
  const tabs = document.querySelectorAll('[data-tab-target]');
  const tabsContents = document.querySelectorAll("[data-tab-content]")
- /*
- menuSection.addEventListener("click",() => {
-     menu.classList.add("current");
-     homePage.remove("current");
-     aboutPage.remove("current");
- })
- aboutSection.addEventListener("click", () => {
-     aboutPage.classList.add("current");
-     homePage.remove("current");
-     menu.remove("current");
- })
- contactSection.addEventListener("click")
- */
+
+
+
 tabs.forEach(tab => {
     tab.addEventListener('click', (e) => {
         const target = document.querySelector(tab.dataset.tabTarget);
